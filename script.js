8002fbbbd8555857cea96fce6c71b1ab2a936d78
Noor <yeminzaw04.com>
2026-01-09 16:13:26 -0800

    Fix bugs and refactor

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// Return randomly one of three values: Rock, Paper, Scissors
-// number => string
-// 3 => 0(rock), 1(paper), 2(scissors)
-
+// Generate a random number to get a computer choice
 let getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3);
     switch(randomNumber) {
@@ -14,22 +11,52 @@ let getComputerChoice = () => {
     }
 }
 
+// Get user input human choice by retrieving the text of the button
 let getHumanChoice = (e) => {
     return e.currentTarget.textContent;
 };
 
+// Game Program
 let playGame = () => {
+    // Set initial scores
     let humanScore = 0;
     let computerScore = 0;
+
+    // Target elements
     let result = document.querySelector('.result');
+    let buttons = document.querySelectorAll('.btn');
+
+    // Create necessary elements before adding to the DOM
     let resultDisplay = document.createElement("p");
     let gameLog = document.createElement('p');
+    let displayScores = document.createElement('p');
+    let finalAnnouncement = document.createElement('p');
+    let startOver = document.createElement('button');
+    startOver.textContent = "Start Over";
 
+    // Check who first get 5 scores
+    let scoreCheck = () => {
+        finalAnnouncement.textContent = "";
+        switch (5) {
+            case humanScore:
+                finalAnnouncement.textContent = "Yay, you win!"
+                buttons.forEach(button => button.disabled = true);
+                result.appendChild(finalAnnouncement);
+                result.appendChild(startOver);
+                break;
+            case computerScore:
+                finalAnnouncement.textContent = "Ah oh, you lost!"
+                buttons.forEach(button => button.disabled = true);
+                result.appendChild(finalAnnouncement);
+                result.appendChild(startOver);
+        }
+    }
 
+    // Main game logic
     let playRound = (humanChoice, computerChoice) => {
         humanChoice = humanChoice.toLowerCase();
 
-        resultDisplay.textContent = `You chose ${humanChoice} and computer chose ${computerChoice}`;
+        resultDisplay.textContent = `You chose ${humanChoice} and computer chose ${computerChoice}.`;
         result.appendChild(resultDisplay);
 
         if (humanChoice === "rock" && computerChoice === "paper") {
@@ -54,26 +81,27 @@ let playGame = () => {
             gameLog.textContent = "It's a tie!"
         }
 
-        resultDisplay.appendChild(gameLog);
+        result.appendChild(gameLog);
     }
 
-    let buttons = document.querySelectorAll('.btn');
+    // Reset DOM when start over button is clicked
+    startOver.addEventListener('click', e => {
+        while (result.lastChild) {
+            result.removeChild(result.lastChild);
+        }
+        buttons.forEach(button => button.disabled = false);
+        humanScore = 0;
+        computerScore = 0;
+    });
+
+    // User choice buttons: when clicked, run game logic, display scores, and check final scores
     buttons.forEach(button => button.addEventListener('click', e => {
     playRound(getHumanChoice(e), getComputerChoice());
+    displayScores.textContent = `Your score is ${humanScore} and computer score is ${computerScore}.`
+    result.appendChild(displayScores);
+    scoreCheck();
     }));
-
-    // Play the game 5 rounds
-    // for (let i = 0; i < 5; i++) {
-    //     playRound(getHumanChoice(), getComputerChoice());
-    // }
-
-    // if (humanScore < computerScore) {
-    //     alert("You lost! Try again");
-    // } else if (humanScore > computerScore) {
-    //     alert("Congratulations! You won.");
-    // } else {
-    //     alert("What a coincidence! It's a tie.")
-    // }
 };
 
+// Run the game
 playGame();
